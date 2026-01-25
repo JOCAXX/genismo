673b1b011971079ca577d973a22cbfccdd75798a
jocax-hue <alexandre.bat36@gmail.com>
2026-01-25 03:52:39 -0300

    Add files via upload

---
+++ Hello.php
@@ -0,0 +1,15 @@
+<html><head></head><body>
+<?php  
+ define( "PA", 3.14);
+ $A=1;
+ $a=2;
+echo  '<p>vamos ver se quebra 
+
+      a linha</p>'  ;
+
+echo  $A.' '.$a.PA."->".( 7 % 3 ) ;
+//phpinfo()
+?>
+
+</body>
+</html>
---
+++ showfotos.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>ShowJocax</title>
+</head>
+    
+<body>
+<?php
+//$pasta = __DIR__ . DIRECTORY_SEPARATOR ."fotosjocax"; 
+
+$files = glob("fotosjocax/*.*");
+for ($i=1; $i<count($files); $i++) { 
+	$num = $files[$i]; 
+	//echo "<br>$num<br>";
+	echo '<br><table border = "1"  align="center">';
+	echo '<tr> <td align = "left">'. $num .'</th></tr>';
+	echo '<tr> <td align = "center">'. '<img   src="'.$num.'" />'."</td></tr>";
+	echo "</table>";
+	
+	//echo '<img   src="'.$num.'" />'; 
+	//echo '<img  height="500" width="500" src="'.$num.'" />'; 
+}
+
+exit;
+//======================================================
+$files = glob("fotosjocax/*.*");
+for ($i=1; $i<count($files); $i++) { 
+	$num = $files[$i]; 
+	echo '	<li--><img alt="random image" src="'.$num.'" />'; 
+}
+exit;
+//====================================
+
+$pasta = __DIR__ . DIRECTORY_SEPARATOR ."fotosjocax"; 
+//echo "<br>$pasta<br>";
+
+$arquivos = glob("$pasta{*.jpg,*.jpg,*.JPG,*.png,*.gif,}", GLOB_BRACE);
+$arquivos = glob("$pasta"."."."*.jpg", GLOB_BRACE);
+$arquivos = "C:\\inetpub\\wwwroot\\SibixWeb\\fotosjocax\\*.jpg";
+$arquivos = "C:\\inetpub\\wwwroot\\SibixWeb\\fotosjocax\\*.jpg";
+var_dump( $arquivos ); // exit;
+
+//foreach($arquivos as $id => $img){
+foreach(glob($arquivos) as $img){
+   //echo "<br>".$img;//exit;
+   echo '<div class="column is-4">
+    <div class="card"> '. $img . '</div>
+  </div>';
+}
+?>
+
+
+
+
+</body>
+</html>
